fe3305b405cd8830ec3143213b3d29050c753d4a
Allan182 <allan.furlani@hotmail.com>
2024-03-04 21:10:28 -0300

    Add List View and Dynamic Widget

--- lib/main.dart
+++ lib/main.dart
@@ -37,52 +37,142 @@ class _WidgetCustomState extends State<WidgetCustom> {
               color: Colors.black,
             )),
       ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            decoration: const BoxDecoration(
-                borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(30)),
-                color: Colors.white),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Lev's Your PC",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      "Produtos",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.grey[40],
-                          hintText: "Busque seus Produto!",
-                          prefixIcon: const Icon(Icons.search),
-                          border: const OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Colors.red, style: BorderStyle.none),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30)))),
-                    )
-                  ]),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: const BoxDecoration(
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(30)),
+                  color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Lev's Your PC",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Produtos",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(120, 183, 183, 183),
+                              borderRadius: BorderRadius.circular(15)),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                                hintText: "Busque seus Produto!",
+                                prefixIcon: Icon(Icons.search),
+                                border: InputBorder.none),
+                          )),
+                      const SizedBox(
+                        height: 10,
+                      )
+                    ]),
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  const Text('Promoção do dia!',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        promoCard('assets/images/fans.webp'),
+                        promoCard('assets/images/water.webp'),
+                        promoCard('assets/images/gabinete.jpg'),
+                        promoCard('assets/images/ram.jpg'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/cover.jpg'))),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: const [
+                                0.2,
+                                0.9
+                              ],
+                              colors: [
+                                Colors.black.withOpacity(0.3),
+                                Colors.black.withOpacity(0.1)
+                              ])),
+                      child: const Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Text('Serviços',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget promoCard(url_image) {
+  return AspectRatio(
+    aspectRatio: 2 / 3,
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(fit: BoxFit.cover, image: AssetImage(url_image)),
+      ),
+      margin: const EdgeInsets.only(right: 15),
+      child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient:
+                  LinearGradient(begin: Alignment.bottomRight, stops: const [
+                0.1,
+                0.9
+              ], colors: [
+                Colors.black.withOpacity(0.8),
+                Colors.black.withOpacity(0.1),
+              ]))),
+    ),
+  );
 }
